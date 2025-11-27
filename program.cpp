7a5a5f528f30dfe776ddca70003f3ceb2bedd242
wiktorw <wiktorw@mat.umk.pl>
2025-11-27 18:40:58 +0100

    Dodanie funkcji fibonacci

--- program.cpp
+++ program.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	
-	int n;
-	cin>>n;
-	cout<<n<<endl;
-	
-	return 0;
+int fibo(int n) {
+    if (n <= 1) 
+	return n;
+    return fibo(n - 1) + fibo(n - 2);
 }
-	
+
+int main() {
+    int n;
+    	cin >> n;
+	cout << n << " " << fibo(n) << endl;
+    return 0;
+}
